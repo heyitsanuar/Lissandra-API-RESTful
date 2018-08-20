4f48c609a04c0e3914b86cf1b38cbe874ef3b309
Anuar Jimenez <heyitsanuar@gmail.com>
2018-08-20 00:29:52 -0500

    Completed wishlist controllers.

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -62,7 +62,41 @@ function addProduct(req, res){
     });
 }
 
+// DELETE '/wishlist/:userId/:productId' Deletes an existing item from the user's wishlist
+function deleteProduct(req, res){
+    //Allocating received variables from params
+    let params = req.params;
+    let userId = params.userId;
+    let productId = params.productId;
+
+    Wishlist.findOne({user: userId, products: { $in: [productId]} }, (err, wishlist) => {
+        if(err)
+            return res.status(500).send({message: 'Error while loading wishlist.'});
+        
+        if(!wishlist)
+            return res.status(404).send({message: 'Wishlist not found.'});
+
+        //Moving products to temporal product array
+        let products = wishlist.products;
+        let indexToRemove = products.indexOf(productId);
+
+        //Removing the index from the array
+        products.splice(indexToRemove, 1);
+        wishlist.products = products;
+        
+        wishlist.save((err, wishlistUpdated) => {
+            if(err)
+                return res.status(500).send({message: 'Error while saving product.'});
+            
+            wishlistUpdated.populate('products', () => {
+                return res.status(200).send({wishlist: wishlistUpdated})
+            });
+        });
+    });
+}
+
 module.exports = {
     getProducts,
-    addProduct
+    addProduct,
+    deleteProduct
 };
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -9,5 +9,6 @@ let WishlistController = require('../controllers/wishlist');
 
 api.get('/wishlist/:userId', MiddlewareAuth.ensureAuth, WishlistController.getProducts);  //Gets the existing products from the given user's wishlist
 api.post('/wishlist/:userId/:productId', MiddlewareAuth.ensureAuth, WishlistController.addProduct); //Adds an item to the user's wishlist
+api.delete('/wishlist/:userId/:productId', MiddlewareAuth.ensureAuth, WishlistController.deleteProduct); //Deletes an existing item from the user's wishlist
 
 module.exports = api;
