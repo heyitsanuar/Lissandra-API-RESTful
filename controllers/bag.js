6d3135cff3c4f7ee84bab7fbcd868e26d8b0c83f
Anuar Jimenez <heyitsanuar@gmail.com>
2018-08-20 00:23:58 -0500

    Added get list of products controller.

--- controllers/bag.js
+++ controllers/bag.js
@@ -3,6 +3,19 @@
 //Models
 let Bag = require('../models/bag');
 
-module.exports = {
+// GET '/bag/:userId' Gets the existing products from the given user's bag
+function getProducts(req, res){
+    //Allocating received variables from params
+    let userId = req.params.userId;
 
+    Bag.find({user: userId}).populate('products').exec((err, bag) => {
+        if(err)
+            return res.status(500).send({message: 'Error while loading bag.'});
+        
+        return res.status(200).send({bag: bag});
+    });
+}
+
+module.exports = {
+    getProducts
 };
--- routes/bag.js
+++ routes/bag.js
@@ -2,5 +2,11 @@
 
 let express = require('express');
 let api     = express.Router();
+//Middlewares
+let MiddlewareAuth = require('../middlewares/authenticated');
+//Controllers
+let BagController = require('../controllers/bag');
+
+api.get('/bag/:userId', MiddlewareAuth.ensureAuth, BagController.getProducts); //Gets the existing products from the given user's bag
 
 module.exports = api;
