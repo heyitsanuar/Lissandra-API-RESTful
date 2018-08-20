ccc21a2e0b665d6de52ffea2719876dd44aaca0f
Anuar Jimenez <heyitsanuar@gmail.com>
2018-08-19 23:36:52 -0500

    Added body parser middleware setup.

--- app.js
+++ app.js
@@ -7,4 +7,8 @@ let bodyParser = require('body-parser');
 //App instance
 let app = express();
 
+//Middlewares
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
 module.exports = app;
