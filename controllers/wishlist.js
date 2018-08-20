c0216e7d80ffdba87e4cdeaadc4e1be4ec57a801
Anuar Jimenez <heyitsanuar@gmail.com>
2018-08-20 00:28:03 -0500

    Added get list of products in wishlist controller.

--- controllers/wishlist.js
+++ controllers/wishlist.js
@@ -3,6 +3,19 @@
 //Models
 let Wishlist = require('../models/wishlist');
 
+// GET '/wishlist/:userId' Gets the existing products from the given user's wishlist
+function getProducts(req, res){
+    //Allocating received variables from params
+    let userId = req.params.userId;
+
+    Wishlist.find({user: userId}).populate('products').exec((err, wishlist) => {
+        if(err)
+            return res.status(500).send({message: 'Error while loading wishlist.'});
+        
+        return res.status(200).send({wishlist: wishlist});
+    });
+}
+
 module.exports = {
-    
+    getProducts
 };
--- routes/wishlist.js
+++ routes/wishlist.js
@@ -2,5 +2,11 @@
 
 let express = require('express');
 let api     = express.Router();
+//Middlewares
+let MiddlewareAuth = require('../middlewares/authenticated');
+//Controllers
+let WishlistController = require('../controllers/wishlist');
+
+api.get('/wishlist/:userId', MiddlewareAuth.ensureAuth, WishlistController.getProducts);  //Gets the existing products from the given user's wishlist
 
 module.exports = api;
